30db8e12ba0f939304ae98cf9de0b7405edb141f
KJH <gh7052@gmail.com>
2024-04-19 01:09:28 +0900

    Add : 위시 리스트 보기 화면 UI 생성

---
+++ WishList/Controller/WishListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WishListViewController.swift
+//  WishList
+//
+//  Created by 김정호 on 4/19/24.
+//
+
+import UIKit
+
+class WishListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WishList/View/Cell/WishListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WishListTableViewCell.swift
+//  WishList
+//
+//  Created by 김정호 on 4/19/24.
+//
+
+import UIKit
+
+class WishListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
